69795fb1050eceab10522ffa49c8b434b585e5e9
kristiancorreia <kristiancorreia@hotmail.com>
2021-04-01 02:40:53 +0000

    Main file now runs everything. Module structure will be a stretch goal after functionailty is established PVC Now Working

--- main.tf
+++ main.tf
@@ -1,3 +1,74 @@
-module "cloud-mysql-server" {
-  source = "./modules/cloudsql-mysql-server"
+resource "google_service_account" "kc-gke-wp-gke-sa" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
+
+resource "google_container_cluster" "kc-gke-wp-gke-cluster" {
+  name     = "kc-gke-wp-gke-cluster"
+  location = "northamerica-northeast1"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  master_auth {
+    username = ""
+    password = ""
+    client_certificate_config {
+        issue_client_certificate = false
+    }
+  }
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block = "/16"
+    services_ipv4_cidr_block = "/22"
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "primary-node-pool"
+  location   = "northamerica-northeast1"
+  cluster    = google_container_cluster.kc-gke-wp-gke-cluster.name
+  node_count = 1
+  management {
+    auto_upgrade = true
+    auto_repair = true
+  }
+  
+    
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.kc-gke-wp-gke-sa.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host = google_container_cluster.kc-gke-wp-gke-cluster.endpoint
+
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.kc-gke-wp-gke-cluster.master_auth[0].cluster_ca_certificate)
+}
+
+resource "kubernetes_persistent_volume_claim" "wordpress-volumeclaim" {
+  metadata {
+    name = "wordpress-volumeclaim"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "200Gi"
+      }
+    }
+  }
 }
