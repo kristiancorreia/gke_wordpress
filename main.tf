c248424ab4f205eb840e4063814b5c97f4f82350
kristiancorreia <kristiancorreia@hotmail.com>
2021-04-06 22:37:03 +0000

    addition of wordpress service

--- main.tf
+++ main.tf
@@ -37,3 +37,9 @@ module "wordpress-deployment" {
   imported_cluster_ca_certificate = module.gke-cluster.cluster_ca_certificate
   imported_cloud_sql_name = module.cloud-sql-server.sql_instance_connection_name
 }
+
+module "wordpress-service" {
+  source = "./modules/wordpress-service"
+  imported_cluster_endpoint = module.gke-cluster.cluster_endpoint
+  imported_cluster_ca_certificate = module.gke-cluster.cluster_ca_certificate
+}
---
+++ modules/wordpress-service/wordpress-service.tf
@@ -0,0 +1,36 @@
+variable "imported_cluster_endpoint" {
+    type = string
+}
+
+variable "imported_cluster_ca_certificate" {
+  type = string
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host = var.imported_cluster_endpoint
+
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(var.imported_cluster_ca_certificate)
+}
+
+resource "kubernetes_service" "wordpress-service" {
+  metadata {
+    name  = "worpress-service"
+    labels = {
+      app = "wordpress"
+    }
+  }
+  spec {
+    type = "LoadBalancer"
+    port {
+      port = "80"
+      target_port = "80"
+      protocol = "TCP"
+    }
+    selector = {
+      app = "wordpress"
+    }
+  }
+}
