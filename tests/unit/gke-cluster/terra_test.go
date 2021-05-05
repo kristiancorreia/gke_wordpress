b5bcfa780e4023c772fb1585f978fa1e80b8011c
Kristian Correia <kristiancorreia@hotmail.com>
2021-05-05 21:07:13 +0000

    Added GKE Cluster test

--- tests/unit/gke-cluster/terra_test.go
+++ tests/unit/gke-cluster/terra_test.go
@@ -1,12 +1,9 @@
 package test
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/gcp"
-	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
@@ -15,15 +12,11 @@ func TestTerraformGcpHelloWorldExample(t *testing.T) {
 
 	projectId := gcp.GetGoogleProjectIDFromEnvVar(t)
 
-	instanceName := fmt.Sprintf("gcp-hello-world-example-%s", strings.ToLower(random.UniqueId()))
-
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "",
 
-		Vars: map[string]interface{}{
-			"instance_name": instanceName,
-		},
+		Vars: map[string]interface{}{},
 
 		EnvVars: map[string]string{
 			"GOOGLE_CLOUD_PROJECT": projectId,
