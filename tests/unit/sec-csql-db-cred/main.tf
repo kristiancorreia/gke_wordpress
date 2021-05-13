7a222b4a71da5d4b12ca7ec96e0c152a558515b5
Kristian Correia <kristiancorreia@hotmail.com>
2021-05-13 05:08:00 +0000

    fix typo in secret-db cred unit test

--- tests/unit/sec-csql-db-cred/main.tf
+++ tests/unit/sec-csql-db-cred/main.tf
@@ -14,6 +14,6 @@ module "sec-csql-db-cred" {
   source = "../../../modules/sec-csql-db-cred"
   imported_cluster_endpoint = module.test-gke-cluster.cluster_endpoint
   imported_cluster_ca_certificate = module.test-gke-cluster.cluster_ca_certificate
-  imported_username = module.cloud-sql-server.sql_instance_username
-  imported_password = module.cloud-sql-server.sql_instance_password
+  imported_username = module.cloudsql-mysql-server.sql_instance_username
+  imported_password = module.cloudsql-mysql-server.sql_instance_password
 }
