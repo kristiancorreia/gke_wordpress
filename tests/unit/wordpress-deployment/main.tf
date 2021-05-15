3fff4ba56b6de6365116c2c2db8ee6a8d94bc9bf
Kristian Correia <kristiancorreia@hotmail.com>
2021-05-15 05:57:50 +0000

    fix issue with wp deployment unit test

--- tests/unit/wordpress-deployment/main.tf
+++ tests/unit/wordpress-deployment/main.tf
@@ -6,15 +6,10 @@ module "test-gke-cluster" {
   source = "../../../modules/test-gke-cluster"
 }
 
-module "service-account" {
-  source = "../../../modules/service-account"
-}
-
-module "sec-csql-inst-cred" {
-  source = "../../../modules/sec-csql-inst-cred"
+module "pvc" {
+  source = "../../../modules/pvc"
   imported_cluster_endpoint = module.test-gke-cluster.cluster_endpoint
   imported_cluster_ca_certificate = module.test-gke-cluster.cluster_ca_certificate
-  imported_sa_key = module.service-account.service_account_key
 }
 
 module "cloudsql-mysql-server" {
